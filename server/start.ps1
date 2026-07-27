4082423360cc0797f632ea23be4aca6ab59b60be
seedling <106108399+seeedl1ng@users.noreply.github.com>
2026-07-28 01:28:14 +0300

    Update start.ps1

--- server/start.ps1
+++ server/start.ps1
@@ -1,17 +1,22 @@
-$Java = "C:\Program Files\Eclipse Adoptium\jre-26.0.1.8-hotspot\bin\java.exe"
-& $Java `
-	'-XX:+UseZGC' `
-	'-XX:+UseLargePages' `
-	'-XX:+DisableExplicitGC' `
-	'-XX:+UseCompactObjectHeaders' `
-	'-XX:+PerfDisableSharedMem' `
-	'--enable-native-access=ALL-UNNAMED' `
-	'--add-opens=java.base/java.lang=ALL-UNNAMED' `
-	'--add-opens=java.base/sun.nio.ch=ALL-UNNAMED' `
-	'-Dlog4j.configurationFile=config/log4j2.xml' `
-	'-Xmx8G' `
-	'-Xms8G' `
-	'-jar' `
-	'cleanroom-0.5.17-alpha.jar' `
+$Java = "C:\Program Files\Eclipse Adoptium\jre-26.0.1.8-hotspot\bin\java.exe" # Replace it for yourself
+$Args = @(
+	'-XX:+UseZGC' 
+	'-XX:+UseLargePages'	 # If you have enabled Large Pages Support / https://docs.oracle.com/en/java/javase/26/docs/specs/man/java.html#large-pages
+	'-XX:+DisableExplicitGC'
+	'-XX:+UseCompactObjectHeaders'
+	'-XX:+PerfDisableSharedMem'
+	# Java 26 arg
+	'--enable-native-access=ALL-UNNAMED'
+	'--add-opens=java.base/java.lang=ALL-UNNAMED'
+	'--add-opens=java.base/sun.nio.ch=ALL-UNNAMED'
+
+	'-Dlog4j.configurationFile=config/log4j2.xml'
+	'-Xmx8G' 	# Replace
+	'-Xms8G'	# Replace
+	'-jar'
+	'cleanroom-0.5.17-alpha.jar'	# Replace
 	'nogui'
+)
+& $Java @Args
+
 Pause
